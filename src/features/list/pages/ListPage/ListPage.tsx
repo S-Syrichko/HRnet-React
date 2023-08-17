cc85dfdc06f3eae4f9724330542752ab8b9ee97e
S-syrichko <s-syrichko@protonmail.com>
2023-08-18 01:49:34 +0200

    feat: Replaced mui-datatables by react-table plugin. Added mocked data for lightouse tests.

--- src/features/list/pages/ListPage/ListPage.tsx
+++ src/features/list/pages/ListPage/ListPage.tsx
@@ -1,109 +1,259 @@
-import { SelectableRows } from "mui-datatables";
-import { Suspense, lazy, useMemo } from "react";
+import { Column, usePagination, useSortBy, useTable } from "react-table";
+import { User } from "../../../../store/features/DataTypes";
 import { useAppSelector } from "../../../../store/store";
 import styles from "./ListPage.module.scss";
-const MUIDataTable = lazy(() => import("mui-datatables"));
+
+const mockUsers: Array<User> = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 2,
+    firstName: "Jack",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 3,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 4,
+    firstName: "Sam",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 5,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 6,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 7,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 8,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 9,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 10,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 11,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+  {
+    id: 12,
+    firstName: "John",
+    lastName: "Doe",
+    startDate: "2021-01-01",
+    department: "Engineering",
+    dateOfBirth: "1990-01-01",
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+  },
+];
+
+const columns: Column<User>[] = [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Start Date",
+        accessor: "startDate",
+      },
+      {
+        Header: "Department",
+        accessor: "department",
+      },
+      {
+        Header: "Date of Birth",
+        accessor: "dateOfBirth",
+      },
+      {
+        Header: "Street",
+        accessor: "street",
+      },
+      {
+        Header: "City",
+        accessor: "city",
+      },
+      {
+        Header: "State",
+        accessor: "state",
+      },
+      {
+        Header: "Zip Code",
+        accessor: "zipCode",
+      },
+    ];
 
 const List = () => {
   const users = useAppSelector((state) => state.users);
+  // Mocked data for performance testing
+  // if (users.length === 0) {
+  //   users = mockUsers;
+  // }
 
-  const columns = useMemo(
-    () => [
-      {
-        label: "First Name",
-        name: "firstName",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Last Name",
-        name: "lastName",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Start Date",
-        name: "startDate",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Department",
-        name: "department",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Date of Birth",
-        name: "dateOfBirth",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Street",
-        name: "street",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "City",
-        name: "city",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "State",
-        name: "state",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-      {
-        label: "Zip Code",
-        name: "zipCode",
-        options: {
-          filter: false,
-          sort: true,
-        },
-      },
-    ],
-    []
-  );
-
-  const options = {
-    filter: false,
-    download: false,
-    print: false,
-    selectableRows: "none" as SelectableRows,
-    viewColumns: false,
-    pagination: false,
-  };
+  const {
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state: { pageIndex},
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,      
+    pageOptions,
+  } = useTable({ columns, data: users }, useSortBy, usePagination);
 
   return (
     <main className={styles.page}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <MUIDataTable
-          title={"Current Employees"}
-          data={users}
-          columns={columns}
-          options={options}
-        />
-      </Suspense>
+      <h2>Current employees</h2>
+      <table className={styles.table}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())} scope="col">
+                {column.render("Header")}
+                <span>
+                  {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {page.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+    <div className={styles.pagination}>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+      </div>
     </main>
   );
 };
